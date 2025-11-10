60f14a0998759bb18736d2eda0edbc73128460e1
RayanSUK <sukkarie.rayan@gmail.com>
2025-11-10 14:38:13 +0100

    refactorisation (appel api concurrent).

--- src/Bal.java
+++ src/Bal.java
@@ -1,21 +1,16 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 // BAL.java
 public class Bal {
-    private String buffer;          // la "lettre"
-    private boolean available=false; // true = pleine
+    private BlockingQueue<String> queue =  new ArrayBlockingQueue<>(1) ;
 
-    public synchronized void deposer(String lettre) throws InterruptedException {
-        while (available) wait();   // attendre que la BAL soit vide (tant que availabe = true ) le producteur attend
-        buffer = lettre;
-        available = true;
-        notifyAll();
+    public boolean deposer(String lettre) throws InterruptedException {
+        return queue.offer(lettre,200,TimeUnit.MILLISECONDS);
     }
 
-    public synchronized String retirer() throws InterruptedException {
-        while (!available) wait();  // attendre qu'il y ait une lettre
-        String out = buffer;
-        buffer = null;
-        available = false;
-        notifyAll();
-        return out;
+    public String retirer() throws InterruptedException {
+        return queue.poll(200,TimeUnit.MILLISECONDS);
     }
 }
